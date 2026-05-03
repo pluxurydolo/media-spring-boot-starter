e9188f3dbfb8190693065c0922470df0fc74f98c
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-05-03 22:10:38 +0300

    03.05.2026

--- src/test/java/com/pluxurydolo/media/configurer/ImageAudioMergerFrameRecorderConfigurerTests.java
+++ src/test/java/com/pluxurydolo/media/configurer/ImageAudioMergerFrameRecorderConfigurerTests.java
@@ -3,20 +3,14 @@ package com.pluxurydolo.media.configurer;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.FFmpegFrameRecorder;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.awt.image.RenderedImage;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.mockito.ArgumentMatchers.anyDouble;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 class ImageAudioMergerFrameRecorderConfigurerTests {
     private static final ImageAudioMergerFrameRecorderConfigurer CONFIGURER =
         new ImageAudioMergerFrameRecorderConfigurer();
@@ -30,25 +24,25 @@ class ImageAudioMergerFrameRecorderConfigurerTests {
         FFmpegFrameRecorder mock = mock(FFmpegFrameRecorder.class);
 
         doNothing()
-            .when(mock).setImageHeight(anyInt());
+            .when(mock).setImageWidth(1);
         doNothing()
-            .when(mock).setImageWidth(anyInt());
+            .when(mock).setImageHeight(1);
         doNothing()
-            .when(mock).setVideoCodec(anyInt());
+            .when(mock).setVideoCodec(27);
         doNothing()
-            .when(mock).setFormat(anyString());
+            .when(mock).setFormat("mp4");
         doNothing()
-            .when(mock).setFrameRate(anyDouble());
+            .when(mock).setFrameRate(30.0);
         doNothing()
-            .when(mock).setVideoBitrate(anyInt());
+            .when(mock).setVideoBitrate(2_000_000);
         doNothing()
-            .when(mock).setAudioCodec(anyInt());
+            .when(mock).setAudioCodec(86018);
         doNothing()
-            .when(mock).setSampleRate(anyInt());
+            .when(mock).setSampleRate(1);
         doNothing()
-            .when(mock).setAudioChannels(anyInt());
+            .when(mock).setAudioChannels(1);
         doNothing()
-            .when(mock).setAudioBitrate(anyInt());
+            .when(mock).setAudioBitrate(128_000);
 
         return mock;
     }
