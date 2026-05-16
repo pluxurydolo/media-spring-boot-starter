047500023a76c33fff30603e0d0e910807199ea7
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-05-16 18:53:36 +0300

    16.05.2026

--- src/integrationTest/java/com/pluxurydolo/media/client/WatermarkClientIntegrationTests.java
+++ src/integrationTest/java/com/pluxurydolo/media/client/WatermarkClientIntegrationTests.java
@@ -39,7 +39,7 @@ class WatermarkClientIntegrationTests extends AbstractIntegrationTests {
 
         assertThat(file)
             .exists()
-            .hasSize(2351431L);
+            .isNotEmpty();
 
         assertThat(logs)
             .hasSize(1);
@@ -59,7 +59,7 @@ class WatermarkClientIntegrationTests extends AbstractIntegrationTests {
 
         assertThat(file)
             .exists()
-            .hasSize(281470L);
+            .isNotEmpty();
 
         assertThat(logs)
             .hasSize(1);
